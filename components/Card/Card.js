24670aa88960570171ed1d5178e1c32d4afb692c
unknown <jose360pelico@gmail.com>
2023-08-02 23:58:37 -0600

    All components were updated

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,14 +1,15 @@
 import "./Card.css";
 
-import React from "react";
+import React, { useState , useEffect } from "react";
+
 
 function Card({ weatherData2 }) {
 
     if (!weatherData2 || !weatherData2.list) {
-        return "Loading..."; // O puedes mostrar un mensaje de carga, por ejemplo.
+        return "Loading..."; 
       }
     
-      // Filtrar los pronósticos de los próximos 5 días
+/*       // Filtrar los pronósticos de los próximos 5 días
       const forecastForNext5Days = weatherData2.list
         .filter((item, index) => index % 8 === 0)
         .slice(0, 5);
@@ -20,7 +21,32 @@ function Card({ weatherData2 }) {
     minTemp: forecastItem.main.temp_min.toFixed(0),
     weatherIcon: forecastItem.weather[0].icon,
   }));
+   */
+  // this is just a test to get differentinformation for the forecast 
+    
+  const [forecastData, setForecastData] = useState([]);
 
+  useEffect(() => {
+    // Filtrar los datos para obtener las cards de hoy y los próximos 4 días
+    const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  const nextFiveDaysData = weatherData2.list.filter((item) => {
+    const itemDate = new Date(item.dt * 1000); // Convertir el timestamp a fecha
+    itemDate.setHours(0, 0, 0, 0);
+    return itemDate >= tomorrow && itemDate <= new Date(tomorrow.getTime() + 4 * 24 * 60 * 60 * 1000); // 5 días en milisegundos
+  });
+  
+    console.log(nextFiveDaysData)
+
+    setForecastData(nextFiveDaysData);
+  }, [weatherData2]);
+   
+
+ // this is the end of the test 
   function formatDate(dateString) {
     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -33,28 +59,37 @@ function Card({ weatherData2 }) {
     return `${dayOfWeek} ${day} ${month}`;
   }
 
-  const fechaCorrecta = weatherData2.list
-  ? formatDate(weatherData2.list[0].dt_txt)
-  : "Loading...";
+
+
 
   return (
     <div className="mainContainerCard">
       {weatherData2 && (
         <>
-          {forecastDataForCards.map((forecastData, index) => (
-            <div className="ContenedorCard" key={index} >
-              <section className="secOne">
-                <h3 className="letraInf">{forecastData.date}</h3>
-                <div className="ContImagen">
-                   <img src="./LightCloud.png" alt="image" /> 
+    
+           {forecastData.map((forecastItem, index) => 
+            {
+              if (index %8 === 0) {
+                return(
+                  <div className="ContenedorCard" key={index} >
+                  <section className="secOne">
+                    <h3 className="letraInf">{formatDate(forecastItem.dt_txt)}</h3>
+                    <div className="ContImagen">
+                       <img src={`/${forecastItem.weather[0].main}.png`} alt="image" /> 
+                    </div>
+                  </section>
+                  <section className="secInformacion">
+                    <h4 className="letraInf"> {forecastItem.main.temp_max.toFixed(0)} °C</h4>
+                    <h4 className="letraInf menor"> {forecastItem.main.temp_min.toFixed(0)}  °C</h4>
+                  </section>
                 </div>
-              </section>
-              <section className="secInformacion">
-                <h4 className="letraInf"> {forecastData.maxTemp} °C</h4>
-                <h4 className="letraInf menor"> {forecastData.minTemp}  °C</h4>
-              </section>
-            </div>
-           ))} 
+                );
+
+              }
+              return null
+            }
+
+           )} 
         </>
       )}
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,229 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import Block from "../../components/Block/Block";
 import Card from "../../components/Card/Card";
-import Navbar from "../../components/NavBar/Navbar";
+import "../../components/NavBar/Navbar.css";
 
-function Buttons (){
-  return(
+function Buttons() {
+  return (
     <div className="GradosOpt">
-        <button>°C</button>
-        <button>°F</button>
+      <button>°C</button>
+      <button>°F</button>
     </div>
+  );
+}
 
-  ) 
+/* this is just a test for the  SEARCH Component */
+
+function Search({ handleChange , handleSearch, setReenderizado}) {
+  /* function to get the input element */
+  const [inputData, setInputData] = useState("");
+  const [newCity , setNewCity] = useState("");
+
+  const handleInput = (e) => {
+    setInputData(e.target.value);
+    setNewCity(e.target.value)
+  };
+
+  const handleSearchButtonClick =() =>{
+    handleSearch(newCity)
+    setReenderizado(true)
+  }
+
+  return (
+    <div className="ContPrin">
+      <section className="FirstSec">
+        <div className="ImCont" >
+          <img src="./close.svg" alt="image" onClick={handleChange}/>
+        </div>
+      </section>
+      {/* Second section for the search menu */}
+      <section className="SecondSec">
+        <div className="Clbtn">
+          <div className="searchImg">
+            <img src="./search1.svg" alt="image" />
+          </div>
+          <input
+            className="busqueda"
+            type="text"
+            placeholder="search location"
+            onChange={handleInput}
+          />
+        </div>
+        <button className="btn2" onClick={handleSearchButtonClick}>Search</button>
+      </section>
+      {/* Third section for the search menu */}
+      <section className="ThirdSec">
+        <div className="div ">
+          <p className="letra" value="London">London</p>
+          <span className="sp active"> &gt; </span>
+        </div>
+        <div className="div">
+          <p className="letra" value="Barcelona">Barcelona</p>
+          <span className="sp active"> &gt; </span>
+        </div>
+        <div className="div">
+          <p className="letra" value="Long Beach">Long Beach</p>
+          <span className="sp active"> &gt; </span>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 
 export default function Home() {
   /* Creating function to get the data */
+  
+  const [weatherData, setWeatherData] = useState();
+  const [forecastData,setForecastData] = useState()
+  const [city, setCity] = useState("Guatemala");
+  const [renderizado , setReenderizado] = useState(false)
 
-    const [weatherData,setWeatherData] = useState ("")
+  const apiKey = "8b5e798bde5cc7b55691b9ca16d8923c";
 
-    const [currentCity , setCurrentCity] = useState("")
+  const APIweather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  const APIforecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
 
-    const getData = async () => {
-      try {
-        const apiKey = '8b5e798bde5cc7b55691b9ca16d8923c'
-        const city = "Helsinki"
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-
-        const dataWeather = await response.json()
-        setWeatherData(dataWeather)
-        console.log(dataWeather)
-
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    const handleSearch =(city) => {
-      setCurrentCity(city)
-      getData(city)
-    }
-
-    
-    useEffect(()=>{
-      getData()
-    },[]);
-
-
-
-
-
-  /* FUNCTION FOR THE FORECAST  */
-
-  const [forecast, setForecast] = useState ("")
-  const Dataforecast = async () => {
+  useEffect (()=>{
+    async function getData (TypeAPI , setState) {
     try {
-      const apiKey = '8b5e798bde5cc7b55691b9ca16d8923c'
-      const city = "Helsinki"
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`)
-
-      const forecast = await response.json()
-      setForecast(forecast)
-      console.log(forecast)
-
+      const response = await fetch(TypeAPI);
+      const data = await response.json();
+      setState(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
+      }
+    getData(APIweather ,setWeatherData)
+    getData(APIforecast, setForecastData)
+    
+    setReenderizado(false)
+
+},[city,renderizado])
+  
+  
+  
+  const handleSearch = (city) => {
+    setCity(city); // Update the currentCity state when the user performs a search
+  };
+
+  
+ 
+  
+  /* Funcion para el renderizado del navbar */
+  const [content , setContent] = useState(true)
+  function handleChange (){
+    setContent(!content)
   }
 
   
-  
-  useEffect(()=>{
-    Dataforecast()
-  },[]);
+  /* FUNCION PARA FORMATEAR LA FECHA QUE ESTA EN LA API */
+    function formatDate(dateString) {
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec",];
 
+    const date = new Date(dateString);
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
 
+    return `${dayOfWeek} ${day} ${month}`;
+  }
+
+  const fechaCorrecta = forecastData && forecastData.list && forecastData.list.length > 0
+  ? formatDate(forecastData.list[0].dt_txt)
+  : "Loading...";
 
   return (
     <main className="MainContainerPage">
       {/* main information along with the nav bar*/}
       <div className="ContenedorNavBarPage">
-        <Navbar weatherData={weatherData} weatherData2={forecast} onSearch={handleSearch}/>
+        <div className="mainCont">
+          {/* Section for the first view */}
+          {weatherData && forecastData && (
+            <>
+              {content ? (
+                <>
+                  <div>
+                    {/* Search button and geo logo */}
+                    <section>
+                      <div className="Location">
+                        <button className="btn" onClick={handleChange}>
+                          Search for places
+                        </button>
+                        <div className="containerLogo">
+                          <img
+                            src="./location.svg"
+                            alt="image"
+                            className="LImagen"
+                          />
+                        </div>
+                      </div>
+                      <div className="principalImg">
+                        <div className="ImagenCont">
+                          <img
+                            src={`/${weatherData.weather[0].main}.png`}
+                            alt="image"
+                            className="IPrin"
+                          />
+                        </div>
+                      </div>
+                    </section>
+                    {/* weather details */}
+                    <section className="SecondSection">
+                      <div className="Ftitle">
+                        <h1 className="letter title">
+                          {weatherData.main.temp.toFixed(0)}
+                        </h1>
+                        <h3 className="letter grados">°C</h3>
+                      </div>
+                      <h2 className="letter subtitle">
+                        {weatherData.weather[0].main}
+                      </h2>
+                      <div className="information">
+                        <p className="letter">Today</p>
+                        <p className="letter">·</p>
+                        {forecastData.list && (
+                          <p className="letter">{fechaCorrecta}</p>
+                        )}
+                      </div>
+                      <div className="ubicacion">
+                        <div className="IMG">
+                          <img src="./geo.svg" alt="Image" className="IMG" />
+                        </div>
+                        <p className="letter city">{weatherData.name}</p>
+                      </div>
+                    </section>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="mainCont">
+                    <Search handleChange={handleChange}  
+                      handleSearch={handleSearch} 
+                      setReenderizado={setReenderizado}/>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </div>
       </div>
-      {/* Cards for the weather */}
+
+      
       <div className="ContenedorInformationPage">
         <div className="contBotones">
-          <Buttons/>
+          <Buttons />
         </div>
         <div className="DivCardspage">
-          <Card weatherData2={forecast}/>
+           <Card weatherData2={forecastData} /> 
         </div>
         <div className="DivBlockpage">
-          <Block weatherData={weatherData}/>
+          <Block  weatherData={weatherData}/>
         </div>
-      </div>   
+      </div>
     </main>
-  )
+  );
 }
